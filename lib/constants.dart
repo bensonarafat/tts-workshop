4240c45f707ffb65e68fe7124a22834fbf69c71e
Benson Arafat <benpaul320@gmail.com>
2025-04-11 23:26:42 +0100

    Update contants variables

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,11 @@ class Contants {
   ///
   /// You need an API key to call the Gemini API. If you don't already have one, create a key in Google AI Studio.
   /// https://aistudio.google.com/app/apikey
-  static String googleAPIKey = "[API KEY HERE]";
+  static String googleAPIKey = "AIzaSyBhFDKtL8Szh-yrFJxzXNnmneTThzziF_I";
+
+  /// Text to Speech API
+  static String googleTextUrl =
+      "https://texttospeech.googleapis.com/v1/text:synthesize?key=$googleAPIKey";
 
   ///
   /// Prompts
@@ -10,8 +14,9 @@ class Contants {
     /// Gemini AI prompts
     "Generate a transcript of the speech.",
     "Provide a transcript of the speech from 00:00 to 00:00.",
+    "Who is the speaker in the audio?"
 
-    /// Google TTS prompts
-    "Hello and welcome! Today, we’re exploring the amazing world of intelligent audio applications. With the power of Google Cloud and Gemini AI, we can teach machines to listen, understand, and even talk back — just like humans. Let’s get started!"
+        /// Google TTS prompts
+        "Hello and welcome! Today, we’re exploring the amazing world of intelligent audio applications. With the power of Google Cloud and Gemini AI, we can teach machines to listen, understand, and even talk back — just like humans. Let’s get started!"
   ];
 }
